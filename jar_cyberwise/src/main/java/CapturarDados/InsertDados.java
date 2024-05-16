4ae39124912e8e1a958c350bbf37052162795013
Davi <davi.mendonca@sptech.school>
2024-05-16 17:57:07 -0300

    Fazendo conexão para banco local da vm e servidor

--- jar_cyberwise/src/main/java/CapturarDados/InsertDados.java
+++ jar_cyberwise/src/main/java/CapturarDados/InsertDados.java
@@ -2,13 +2,17 @@ package CapturarDados;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import ConexaoBanco.Conexao;
+import ConexaoBanco.ConexaoLocal;
 
 import java.util.List;
 
 public class InsertDados {
     public static void inserirBanco(Componentes componentes, Integer id) {
         Conexao conexao = new Conexao();
+        ConexaoLocal conexaoLocal = new ConexaoLocal();
+
         JdbcTemplate con = conexao.getConexaoDoBanco();
+        JdbcTemplate con1 = conexaoLocal.getConexaoDoBanco();
 
 
         long totalDisco = componentes.getTotalDisco();
@@ -25,12 +29,15 @@ public class InsertDados {
 
                 con.execute("INSERT INTO monitoramento (cpuEmUso, ramDisponivel, tamanhoDisponivelDisco, fk_maquina, data_hora) " +
                         "VALUES ("+ cpuEmUso + ", " + ramDisponivel + ", " + tamanhoDisponivel + ", " + id + ", CURRENT_TIMESTAMP)");
+
+
+                con1.execute("INSERT INTO monitoramento (cpuEmUso, ramDisponivel, tamanhoDisponivelDisco, fk_maquina, data_hora) " +
+                        "VALUES ("+ cpuEmUso + ", " + ramDisponivel + ", " + tamanhoDisponivel + ", " + id + ", CURRENT_TIMESTAMP)");
 //         lembrar de colocar rede
 
 //         con.execute("INSERT INTO monitoramento (dadosCPU, dadosRAM, dadosDISCO, dadosREDE, fk_maquina, data_hora) " +
 //                 "VALUES ("+ cpuEmUso + ", " + ramDisponivel + ", " + tamanhoDisponivel + ", 4.2, 1, CURRENT_TIMESTAMP)");
 
-//         List<Componentes> componentesDoBanco = con.query("SELECT * FROM monitoramento", new BeanPropertyRowMapper<>(Componentes.class));
                 System.out.println("Coletando...");
 
                 Thread.sleep(5000);
--- jar_cyberwise/src/main/java/ConexaoBanco/ConexaoLocal.java
+++ jar_cyberwise/src/main/java/ConexaoBanco/ConexaoLocal.java
@@ -3,24 +3,24 @@ package ConexaoBanco;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-public class Conexao {
+public class ConexaoLocal {
     private JdbcTemplate conexaoDoBanco;
 
-    public Conexao(){
+    public ConexaoLocal(){
         BasicDataSource dataSource = new BasicDataSource();
+
+        BasicDataSource dataSource1 = new BasicDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://34.229.136.76:3306/cyberwise");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/cyberwise");
 //        dataSource.setUsername("root");
         dataSource.setUsername("cyberwise");
         dataSource.setPassword("cyber100");
 //        dataSource.setPassword("1513");
 
-        conexaoDoBanco = new JdbcTemplate(dataSource);
-
+        conexaoDoBanco = new JdbcTemplate(dataSource1);
     }
 
     public JdbcTemplate getConexaoDoBanco() {
         return conexaoDoBanco;
     }
 }
-
