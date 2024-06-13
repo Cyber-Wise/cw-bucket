8f83229475fe1247b343cd6e8ae2072ff5b79bbf
Ubuntu <ubuntu@ip-172-31-21-72.ec2.internal>
2024-06-13 15:11:27 +0000

    iniciando automatico

--- exec.sh
+++ exec.sh
@@ -7,17 +7,10 @@ cd /home/ubuntu/cw-bucket/cyberwise
 echo "Atualizando o repositório..."
 git pull
 
-# Executar o JAR com expect
-expect << EOF
-spawn java -jar jar_cyberwise.jar
-expect "Jar iniciando..."
-send "vision@cyberwise.com\r"
-expect "Insira sua senha:"
-send "vision@123\r"
-expect "Logado com sucesso"
-expect "Bem Vindo(a) Vision"
-expect "Insira o código de acesso da máquina:"
-send "201\r"
-expect "Deseja começar o monitoramento? (s/n)"
-send "s\r"
+# Executar o JAR em primeiro plano
+java -jar jar_cyberwise.jar <<EOF
+vision@cyberwise.com
+vision@123
+201
+s
 EOF
